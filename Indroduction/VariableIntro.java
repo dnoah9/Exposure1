38326c488b881111752320a46204a1d9362cf1f4
15020 <dylan.noah@vtsd.com>
2025-05-27 13:55:19 -0400

    commit

---
+++ Indroduction/VariableIntro.java
@@ -0,0 +1,18 @@
+package Indroduction;
+
+public class VariableIntro {
+    public static void main(String[] args) {
+
+        double salary = 10000.56;
+        int bonus = 250;
+
+        bonus = (int) salary;
+
+        System.out.println(bonus);
+        System.out.println(salary);
+    }
+
+
+
+
+}
