a1a76c755b2045fd364927a49d64f379ba106ab3
15020 <dylan.noah@vtsd.com>
2025-05-30 11:12:55 -0400

    commit

--- Lab/lab100.java
+++ Lab/lab100.java
@@ -1,31 +1,34 @@
 package Lab;
 public class lab100 {
-        public static void main(String[] args) {
-            System.out.println("Lab, 100 point version\n");
 
-            int seconds = 10000;
-            int hours = seconds / 3600;
-            int remainder = seconds % 3600;
-            int minutes = remainder / 60;
-            int remainderSec = remainder % 60;
-            int milliseconds = seconds%1000;
-            int startingMilliseconds =100023;
-            int Milliseconds = startingMilliseconds / 1000;
-            System.out.println("Starting seconds: \t" + seconds);
-            System.out.println("hours: \t\t\t\t" + hours);
-            System.out.println("minutes: \t\t\t" + minutes);
-            System.out.println("remainderSec: \t\t" + remainderSec);
-            System.out.println("milliseconds: \t" + milliseconds);
-            System.out.println(hours +  " hours " + minutes + " minutes " + remainderSec + " seconds ");
+    public static void main(String[] args) {
+        System.out.println("Lab, 100 point version\n");
+
+        int Starting_seconds= 10000;
+        int hours= Starting_seconds/3600;
+        int remainder = Starting_seconds % 3600;
+        int minutes = remainder/60;
+        int seconds = remainder%60;
+        int Starting_milliseconds=10000123;
+        int milliseconds = Starting_milliseconds%1000;
+        System.out.println(" Starting seconds: \t"+ Starting_seconds);
+        System.out.println(" Hours: \t\t\t\t"+ hours);
+        System.out.println(" Minutes: \t\t\t   "+ minutes);
+        System.out.println(" Seconds : \t\t\t   "+ seconds);
+        System.out.println(" Milliseconds: \t\t   "+ milliseconds);
+        System.out.println("10000 seconds equals:");
+        System.out.println( hours + " hours, " +minutes + " minutes, " + seconds + " seconds, " + milliseconds + " milliseconds" );
+
+
+
+
+
+    }
 
 
 
 
         }
 
-
-
-
-    }
 
 
