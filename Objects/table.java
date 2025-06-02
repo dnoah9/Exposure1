0f998c02497ba37f40f66bca62f1870f90791fd7
15020 <dylan.noah@vtsd.com>
2025-06-02 13:55:25 -0400

    commit

---
+++ Objects/table.java
@@ -0,0 +1,152 @@
+package Objects;
+
+
+// since this is European furniture, its measured in cm and kg
+
+public class table {
+
+    double height;
+    double width;
+    double weight;
+    double price;
+    double length;
+
+
+ // default constructor
+    public table(){
+        height = 23;
+        width = 40;
+        weight = 52;
+        price = 50;
+        length = 936;
+    }
+
+
+public double getHeight(){
+        return height;
+}
+
+public double getWidth(){
+        return width;
+}
+
+public double getWeight(){
+        return weight;
+}
+
+public double getPrice(){
+        return price;
+}
+public double getLength(){
+        return length;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
